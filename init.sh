432bbbb363bdaefd17b7c7ceb75617b04d226092
Юрий <y.but@outlook.com>
2020-05-14 15:05:44 +0300

    Данные для урока по WSGI приложениям.

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo /etc/init.d/gunicorn restart
