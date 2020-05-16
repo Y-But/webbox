c64d15bef4f55f46fda4004eeb0c94621558781b
Юрий <y.but@outlook.com>
2020-05-16 20:06:04 +0300

    Правки рабочих файлов под задание. Попытка 2

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from ask.views import not_found
+from ask.views import found, not_found
 
 urlpatterns = [
 
